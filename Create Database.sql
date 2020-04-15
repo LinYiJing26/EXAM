73f2faa53a6f9149903c49f1e31388447ebb0a58
LinYiJing26 <chensw279@nenu.edu.cn>
2020-04-15 13:54:40 +0800

    create database

--- Create Database.sql
+++ Create Database.sql
@@ -1,32 +1,3 @@
-# EXAM
-
-### 登录 （陈 LinYiJing26、秦 resposiii）
-1.教师和学生 登录/改密/注册
-2.管理员 登录/改密 （主）
-
-### 学生 （傅 josartfu17）
-1.个人信息管理 （主）
-**2.考试**
-3.查询成绩
-
-### 教师 （孙 SheldorKop）
-1.个人信息管理 （主）
-2.试卷管理
-3.查询成绩
-
-### 管理员 （许 Darrenreally）
-1.信息管理 （主）
-2.试卷管理
-3.查询成绩
-
-#### 工具
-1.GitHub Desktop https://desktop.github.com/
-2.IntelliJ IDEA 2020.1 x64 https://www.jetbrains.com/idea/download/
-3.MySQL 5.7 https://www.mysql.com/
-4.MySQL Workbench https://dev.mysql.com/downloads/workbench/
-5.apache Tomcat 9.0.20 http://tomcat.apache.org/
-
-#### 数据库
 CREATE DATABASE EXAM;
 USE EXAM;
 CREATE TABLE Student
